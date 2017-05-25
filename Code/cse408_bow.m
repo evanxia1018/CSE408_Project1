4a3edb80244aeafdb0edc93da646939013779dc3
esyee89 <erockyee89@gmail.com>
2017-05-24 20:36:04 -0700

    Update cse408_bow.m

--- Code/cse408_bow.m
+++ Code/cse408_bow.m
@@ -1,7 +1,6 @@
 % function to create a vocabulary from multiple text files under folders
 
 function feat_vec = cse408_bow(filepath, voc)
-
 [fid, msg] = fopen(filepath, 'rt');
 error(msg);
 line = fgets(fid); % Get the first line from
@@ -11,6 +10,25 @@ feat_vec = zeros(size(voc)); %Initialize the feature vector'
 while line ~= -1
 
     %PUT YOUR IMPLEMENTATION HERE
+    %compare list of words in "voc" to "feat_vec"
+    
+    
+        %parse the text file for string values
+        tline=textscan(fid,'%s')
+        %store the string values in cell array
+        lexicons=tline{1}
+        %identify the unique words and store in matrix
+        [x,y,z]=unique(lexicons);
+        %store only the unique names from filepath
+        compthis=[x];
+        %convert to all lowercase for proper comparison
+        newcompthis = lower(compthis);
+        newvoc = lower(voc);
+        
+        bagofwords = any(ismember(newvoc, newcompthis));
+      
     
 end
 fclose(fid);
+disp(bagofwords)
+end
